31b8b9db1b1eb4e1fe6be144f5ee3a236dd9abcb
Sergey Idelson <Sergey.I@debitsuccess.com>
2020-01-06 08:04:50 +1300

    Added a new question about strings

---
+++ CSharpQuiz.Questions.12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpQuiz.Questions._12
+{
+    class Program
+    {
+        public static readonly string str = "Current string is 1";
+
+        static void Main(string[] args)
+        {
+            var str2 = "Current string is 1";
+            Console.WriteLine(object.ReferenceEquals(str2, str));
+
+            var counter = 1;
+            str2 = $"Current string is {counter}";
+            Console.WriteLine(object.ReferenceEquals(str2, str));
+
+            Console.ReadKey();
+        }
+    }
+}
